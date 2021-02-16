cf2ee3c5ddb92414c8e2842c8a5cb87c552fd72d
Joe Lyons <josephjlyons90@gmail.com>
2021-02-16 14:42:44 -0500

    user prompt questions

--- readMe.js
+++ readMe.js
@@ -1,3 +1,76 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const generateMD = require('./utils/generateReadme');
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "LinkedIn",
+            message: "Please enter your LinkedIn URL: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+};
+promptUser()
