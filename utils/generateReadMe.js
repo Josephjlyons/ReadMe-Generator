39888f3d5f59de1c9532feb963a62d93dce4a5f4
Joe Lyons <josephjlyons90@gmail.com>
2021-02-18 14:47:12 -0500

    fill in temp literals

--- utils/generateReadMe.js
+++ utils/generateReadMe.js
@@ -1,9 +1,46 @@
 // Generate ReadMe functions for populating file
 
-function generateMd(answers){
+function generateMd(answers) {
     return ` 
+    # ${answers.projectTitle}
+
+    ## Description
+    ${answers.description}
+
+
+    ## Table of Contents 
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    - [Contact](#contact)
+
+
+    ## Installation
+    ${answers.installation}
+
+
+    ## License 
+    This application is covered by the ${answers.license} license.
+
+    ## Contributors 
+    ${answers.contributing}
+
+    ## Tests 
+    ${answers.tests}
+
+    ## Questions 
+    ${answers.questions}
+
+    ## Contact
+    ${answers.email}
+    ${answers.linkedin}
     
-    `
+
+    `;
 }
 
 module.exports = generateMd
